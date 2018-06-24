dac4656ec8b2eff3cfe886fdc05fc0eb394271e4
andy le <anhlt1983@gmail.com>
2018-06-24 11:41:11 +1000

    initial commit

---
+++ content/meta/config.js
@@ -0,0 +1,32 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "Any Le - A programmer blog", // <title>
+  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
+  siteDescription: "A programmer blog.",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "Andy Le",
+  authorTwitterAccount: "greglobinski",
+  // info
+  infoTitle: "Andy Le",
+  infoTitleNote: "personal blog",
+  // manifest.json
+  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.background,
+  manifestThemeColor: colors.background,
+  manifestDisplay: "standalone",
+  // contact
+  contactEmail: "anhlt1983@gmail.com",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
